f64df024ed69a641b3ed5290bf8dc3ab1ced3706
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-24 16:09:26 +0000

    Create structure of Home component

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const cardsInfo = [
+    {
+      title: "Outdoor Photo",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem necessitatibus porro veritatis dignissimos corporis dolorum, reprehenderit, adipisci illo numquam magni iusto facere voluptate eligendi incidunt accusamus cumque nobis beatae molestias?",
+    },
+    {
+      title: "Outdoor Photo",
+      description:
+        "Harum ad ipsam quis enim? Maxime perferendis fugiat enim. Cumque aliquam possimus quis esse praesentium maiores atque corporis sunt eum, neque placeat!",
+    },
+    {
+      title: "Outdoor Photo",
+      description:
+        "Fugiat aliquam itaque, iusto, non quasi laborum harum tempore nesciunt deleniti modi, fugit aperiam recusandae facere debitis sapiente laudantium odio cum praesentium!",
+    },
+    {
+      title: "Outdoor Photo",
+      description:
+        "Ipsam dignissimos error porro ipsa consequuntur nesciunt tenetur ab, asperiores odit eius labore! Vitae ut consequuntur excepturi nam obcaecati ex? Itaque, temporibus?",
+    },
+  ];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+  return (
+    <div>
+      <Navbar />
+	  <div className="container">
+		<Jumbotron />
+		<div className="row">
+			{cardsInfo.map((cardInfo, index) => (
+				<div className="col-12 col-lg-3" key={index}>
+					<Card 
+						cardTitle={cardInfo.title}
+						cardDescription={cardInfo.description}
+					/>
+				</div>
+			))}
 		</div>
-	);
+	  </div>
+	  <Footer />
+    </div>
+  );
 };
 
 export default Home;
